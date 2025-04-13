e1946f39f4d0840495b1e624422d9d327cb0d281
Barani <baraniofficial7@gmail.com>
2025-04-13 18:47:59 +0530

    Data model change

--- frontend/src/app/components/registration/registration.component.ts
+++ frontend/src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RegistrationProfile } from 'src/app/model/registration-profile';
+import { RegistrationProfile, RequestProfile } from 'src/app/model/registration-profile';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NativeDateAdapter } from '@angular/material/core';
 import { RegisterService } from 'src/app/services/register.service';
@@ -16,6 +16,7 @@ export class RegistrationComponent {
     private registerService:RegisterService) {}
 
   registrationProfile: RegistrationProfile = new RegistrationProfile;
+  requestProfile: RequestProfile = new RequestProfile;
   registrationForm !: FormGroup;
 
   ngOnInit(){
@@ -43,24 +44,25 @@ export class RegistrationComponent {
   }
 
   onSubmit(){
-    this.registrationProfile.name=this.registrationForm.get("name")?.value;
-    this.registrationProfile.email=this.registrationForm.get("email")?.value;
-    this.registrationProfile.contactNumber=this.registrationForm.get("contactNumber")?.value;
-    this.registrationProfile.gender=this.registrationForm.get("gender")?.value;
+    this.requestProfile.name=this.registrationForm.get("name")?.value;
+    this.requestProfile.email=this.registrationForm.get("email")?.value;
+    this.requestProfile.phone_number=this.registrationForm.get("contactNumber")?.value;
+    this.requestProfile.gender=this.registrationForm.get("gender")?.value;
     let date=this.dateAdapter.format(this.registrationForm.get("dob")?.value,'MM/dd/yyyy');
-    this.registrationProfile.dob=date;
-    this.registrationProfile.street=this.registrationForm.get("street")?.value;
-    this.registrationProfile.city=this.registrationForm.get("city")?.value;
-    this.registrationProfile.pincode=this.registrationForm.get("pincode")?.value;
-    this.registrationProfile.state=this.registrationForm.get("state")?.value;
-    this.registrationProfile.nameOnTheBib=this.registrationForm.get("nameOnTheBib")?.value;
-    this.registrationProfile.tshirtSize=this.registrationForm.get("tshirtSize")?.value;
-    this.registrationProfile.nameOfYourGroup=this.registrationForm.get("nameOfYourGroup")?.value;
-    this.registrationProfile.breakfast=this.registrationForm.get("breakfast")?.value;
-    this.registrationProfile.bloodGroup=this.registrationForm.get("bloodGroup")?.value;
-    this.registrationProfile.emergencyContactName=this.registrationForm.get("emergencyContactName")?.value;
-    this.registrationProfile.emergencyContactNumber=this.registrationForm.get("emergencyContactNumber")?.value;
-    this.registrationProfile.termsAndCondition=this.registrationForm.get("termsAndCondition")?.value;
+    this.requestProfile.dob=date;
+    this.requestProfile.street=this.registrationForm.get("street")?.value;
+    this.requestProfile.city=this.registrationForm.get("city")?.value;
+    this.requestProfile.pincode=this.registrationForm.get("pincode")?.value;
+    this.requestProfile.state=this.registrationForm.get("state")?.value;
+    this.requestProfile.name_on_the_bib=this.registrationForm.get("nameOnTheBib")?.value;
+    this.requestProfile.event_type='21km';
+    this.requestProfile.t_shirt_size=this.registrationForm.get("tshirtSize")?.value;
+    this.requestProfile.name_of_your_group=this.registrationForm.get("nameOfYourGroup")?.value;
+    this.requestProfile.breakfast=this.registrationForm.get("breakfast")?.value;
+    this.requestProfile.blood_group=this.registrationForm.get("bloodGroup")?.value;
+    this.requestProfile.emergency_contact_name=this.registrationForm.get("emergencyContactName")?.value;
+    this.requestProfile.emergency_contact_number=this.registrationForm.get("emergencyContactNumber")?.value;
+    this.requestProfile.terms_and_condition=this.registrationForm.get("termsAndCondition")?.value;
     this.registerService.registerUser(this.registrationProfile).subscribe( {
       next:(res)=>{
         console.log('Saved Sucessfully.');
--- frontend/src/app/model/registration-profile.ts
+++ frontend/src/app/model/registration-profile.ts
@@ -20,3 +20,25 @@ export class RegistrationProfile{
     termsAndCondition:any;
     
   } 
+
+  export class RequestProfile{
+    name: any;
+    email: any;
+    phone_number: any;
+    amount: any;
+    event_type: any;
+    gender: any;
+    dob: any;
+    street: any;
+    city: any;
+    pincode: any;
+    state: any;
+    name_on_the_bib: any;
+    t_shirt_size: any;
+    name_of_your_group: any;
+    breakfast: any;
+    blood_group: any;
+    emergency_contact_name: any;
+    emergency_contact_number: any;
+    terms_and_condition: any;
+  }
